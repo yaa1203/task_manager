cfff74404c840cee5c92f62efc16006794a80180
yaa1203 <muhammadilyahamzah@gmail.com>
2025-11-21 10:23:16 +0700

    waktu

--- resources/views/admin/workspace/tasks/show.blade.php
+++ resources/views/admin/workspace/tasks/show.blade.php
@@ -279,13 +279,10 @@
                                     </svg>
                                 </div>
                                 <div>
-                                    @foreach($task->submissions as $submission)
-                                        <p class="text-xs text-gray-500 mb-0.5">Dibuat</p>
-                                        <p class="text-sm font-medium text-gray-900">
-                                        {{ \Carbon\Carbon::parse($submission->created_at)->locale('id')->translatedFormat('d F Y H:i') }}
-
-                                        </p>
-                                    @endforeach
+                                    <p class="text-xs text-gray-500 mb-0.5">Dibuat</p>
+                                    <p class="text-sm font-medium text-gray-900">
+                                        {{ \Carbon\Carbon::parse($task->created_at)->locale('id')->translatedFormat('d F Y H:i') }}
+                                    </p>
                                 </div>
                             </div>
                             <div class="flex items-center gap-3">
